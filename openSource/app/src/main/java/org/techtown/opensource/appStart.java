93f671de78de2ec03453352e4eea20f11e3dfcb3
Sunghyun Do <sunghyundo98@gmail.com>
2022-05-20 11:11:05 +0900

    안드로이드 초안

---
+++ openSource/app/src/main/java/org/techtown/opensource/appStart.java
@@ -0,0 +1,14 @@
+package org.techtown.opensource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class appStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_start);
+    }
+}
