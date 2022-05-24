54c18b7ca6cc241352aae064c0795e87bf713aa1
Sunghyun Do <sunghyundo98@gmail.com>
2022-05-24 21:14:37 +0900

    회원가입 및 로그인 + 로딩 추가

---
+++ openSource/app/src/main/java/org/techtown/opensource/loading.java
@@ -0,0 +1,31 @@
+package org.techtown.opensource;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(loading.this, LoginActivity.class);
+                startActivity(intent);  //Loagin화면을 띄운다.
+                finish();   //현재 액티비티 종료
+            }
+        },2000); // 화면에 Logo 2초간 보이기
+    }
+}
